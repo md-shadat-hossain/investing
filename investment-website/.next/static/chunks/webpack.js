ebbd99a65549cf9894e8409a5a14e3058797de2f
Md. Shadat Hossain <119412677+shadat-hossan@users.noreply.github.com>
2026-02-10 14:33:13 +0600

    remot .next file

--- investment-website/.next/static/chunks/webpack.js
+++ investment-website/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "192c5ff01db9d7aa"; }
+/******/ 		__webpack_require__.h = function() { return "0d653bfdf5f8110a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
